fbef37003c164b42e4e40403989ab1129551708f
이지현 <73107000+dlwlgus5533@users.noreply.github.com>
2023-10-13 12:28:44 +0900

    녹음 오디오플레이 기초

--- components/voiceRecorder/VoiceRecoder.js
+++ components/voiceRecorder/VoiceRecoder.js
@@ -1,13 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Button, View, Text} from "react-native";
+import {StyleSheet, Button, View, Text, Alert} from "react-native";
 import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+import RNFS from 'react-native-fs';
+import axios from "axios";
 
 
 
 function VoiceRecorder() {
     const [audioRecorderPlayer] = useState(new AudioRecorderPlayer());
+
     const [isRecording, setIsRecording] = useState(false);
     const [audioPath, setAudioPath] = useState('');
+    const [contentsAudio, setContentsAudio] = useState('');
 
     const onStartRecord = async () => {
         const result = await audioRecorderPlayer.startRecorder();
@@ -21,13 +25,57 @@ function VoiceRecorder() {
         setAudioPath(result);
     };
 
-    const onStartPlay = async () => {
-        await audioRecorderPlayer.startPlayer(audioPath);
+    const onStartPlay = async (audio) => {
+        if (!!audio) {
+            await audioRecorderPlayer.startPlayer(audio);
+        } else {
+            const cancelButton = {
+                text: '확인',
+                onPress: () => {},
+                style: 'cancel',
+            };
+            const options = {
+                cancelable: true,
+                onDismiss: () => {},
+            };
+            Alert.alert("오류", "재생할 오디오가 존재하지 않습니다.", [cancelButton], options);
+        }
     };
 
     const onStopPlay = async () => {
         await audioRecorderPlayer.stopPlayer();
     };
+
+    const onContentsDownload = () => {
+        const downloadFileName = "test.wav";
+        axios.get('http://192.168.1.70:8080/api/external/files/audio', {
+            params: { selectedFileName: downloadFileName },
+            headers: { 'Content-Type': 'audio/wav' },
+            responseType: 'arraybuffer',
+        })
+            .then(async (response) => {
+                const fileData = new Blob([response.data], { type: 'audio/wav' });
+
+                const filePath = await saveBlobToFile(RNFS.CachesDirectoryPath + '/test.wav', fileData);
+
+                if (filePath) {
+                    setContentsAudio(filePath);
+                }
+            })
+            .catch((error) => {
+                console.error('Error get audio file:', error);
+            });
+    }
+    const saveBlobToFile = async (filePath, fileData) => {
+        try {
+            await RNFS.writeFile(filePath, fileData, 'base64');
+            return filePath;
+        } catch (error) {
+            console.error('saveBlobToFile Error: ', error);
+            return null;
+        }
+    }
+
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -36,8 +84,11 @@ function VoiceRecorder() {
                 title={isRecording ? '녹음 중지' : '녹음 시작'}
                 onPress={isRecording ? onStopRecord : onStartRecord}
             />
-            <Button title="재생 시작" onPress={onStartPlay} />
+            <Button title="재생 시작" onPress={() => onStartPlay(audioPath)} />
             <Button title="재생 중지" onPress={onStopPlay} />
+            <Button title="콘텐츠 다운" onPress={onContentsDownload} />
+            <Button title="콘텐츠 삭제" onPress={onStopPlay} />
+            <Button title="콘텐츠 재생" onPress={() => onStartPlay(contentsAudio)} />
         </View>
     );
 }
